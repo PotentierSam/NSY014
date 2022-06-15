3c6de1b3bc38c169a0c34413d0d7e92f51629f81
PotentierSam <sam.potentier@sts-sio-caen.info>
2022-06-15 23:28:07 +0200

    finished

--- src/main/java/fr/sam/NSY014/Main.java
+++ src/main/java/fr/sam/NSY014/Main.java
@@ -10,9 +10,9 @@ public class Main {
             System.out.println("Erreur : Le nombre minimum d'arguments est 4. Dans l'ordre, il faut l'hôte, le port, l'utilisateur et le mot de passe");
         }
         else {
-            final SquashTMRestApi authSystem = new SquashTMRestApi(args[0], args[1], args[2], args[3]);
-            System.out.println(authSystem.getCampaignById(138));
-
+            final SquashTMRestApi squash = new SquashTMRestApi(args[0], args[1], args[2], args[3]);
+            squash.getTestsByProjectName("Test Project-1","Test 1", "test 1");
+            squash.modifyTestStatus("2","SUCCESS");
         }
     }
 }
--- src/main/java/fr/sam/NSY014/SquashTMRestApi.java
+++ src/main/java/fr/sam/NSY014/SquashTMRestApi.java
@@ -2,9 +2,13 @@ package fr.sam.NSY014;
 
 import groovyjarjarantlr4.v4.runtime.misc.NotNull;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class SquashTMRestApi {
@@ -13,7 +17,7 @@ public class SquashTMRestApi {
     private final String user;
     private final String password;
 
-    public SquashTMRestApi(@NotNull String host ,@NotNull  String port, @NotNull String user, @NotNull String password) {
+    public SquashTMRestApi(@NotNull String host, @NotNull String port, @NotNull String user, @NotNull String password) {
         this.host = host;
         this.port = port;
         this.user = user;
@@ -27,47 +31,114 @@ public class SquashTMRestApi {
         RestAssured.useRelaxedHTTPSValidation();
     }
 
-    public String getCampaignById(@NotNull int campaignId) {
-        auth();
-        String response;
-        String test;
+    public HashMap<String, String> getTestsByProjectName(@NotNull String ProjectName, @NotNull String CampaignName, @NotNull String IterationName) {
+        try {
+            auth();
+            Response response = RestAssured.get("http://" + host + ":" + port + "/squash/api/rest/latest/projects");
+            ArrayList<?> ids;
+            ArrayList<?> names;
+            ids = response.jsonPath().getJsonObject("_embedded.projects.id");
+            names = response.jsonPath().getJsonObject("_embedded.projects.name");
+            String ProjectId;
+            String CampaignId;
+            String IterationId;
+            HashMap<String, String> Tests = new HashMap<String, String>();
+            String info;
 
-        try
-        {
-            response = RestAssured.get("http://" + host + ":" + port + "/squash/api/rest/latest/campaigns/"+campaignId).jsonPath().getJsonObject("name");
-            System.out.println(response.toString());
-            test = RestAssured.get("http://" + host + ":" + port + "/squash/api/rest/latest/iterations/85").jsonPath().getJsonObject("name");
-            System.out.println(test.toString());
-        }
-        catch (Exception exception)
-        {
-            System.out.println(exception.getClass().getSimpleName());
-            switch (exception.getClass().getSimpleName())
-            {
-                case "UnknownHostException":
-                {
-                    System.out.println("Erreur : l'hôte renseigné ne réponds pas");
-                }
 
-                case "ConnectException":
-                {
-                    System.out.println("Erreur : le port renseigné est invalide");
-                }
+            for (int numProject = 0; numProject < names.size(); numProject++) {
+                if (names.get(numProject).toString().equals(ProjectName)) {
+                    ProjectId = ids.get(numProject).toString();
+                    response = RestAssured.get("http://" + host + ":" + port + "/squash/api/rest/latest/projects/" + ProjectId + "/campaigns");
 
-                case "JsonPathException":
-                {
-                    System.out.println("Erreur : le/les identifiant(s) renseigné(s) est/sont invalide(s)");
+                    ids = response.jsonPath().getJsonObject("_embedded.campaigns.id");
+                    names = response.jsonPath().getJsonObject("_embedded.campaigns.name");
+                    info = ("Nom du projet : '" + ProjectName + "' Id du projet : '" + ProjectId + "'\n");
+
+                    for (int numCampaign = 0; numCampaign < names.size(); numCampaign++) {
+                        if (names.get(numCampaign).toString().equals(CampaignName)) {
+                            CampaignId = ids.get(numCampaign).toString();
+                            response = RestAssured.get("http://" + host + ":" + port + "/squash/api/rest/latest/campaigns/" + CampaignId + "/iterations");
+
+                            ids = response.jsonPath().getJsonObject("_embedded.iterations.id");
+                            names = response.jsonPath().getJsonObject("_embedded.iterations.name");
+                            info = info + ("    Nom de campagne : '" + CampaignName + "' Id de campagne : '" + CampaignId + "'\n");
+
+                            for (int numIteration = 0; numIteration < names.size(); numIteration++) {
+                                if (names.get(numIteration).toString().equals(IterationName)) {
+                                    IterationId = ids.get(numIteration).toString();
+                                    response = RestAssured.get("http://" + host + ":" + port + "/squash/api/rest/latest/iterations/" + IterationId + "/test-suites");
+
+                                    ids = response.jsonPath().getJsonObject("_embedded.test-suites.id");
+                                    names = response.jsonPath().getJsonObject("_embedded.test-suites.name");
+                                    info = info + ("        Nom d'iteration : '" + IterationName + "' Id d'iteration : '" + IterationId + "'\n");
+
+                                    for (int numSuiteTest = 0; numSuiteTest < names.size(); numSuiteTest++) {
+                                        String id = ids.get(numSuiteTest).toString();
+                                        String name = names.get(numSuiteTest).toString();
+                                        Tests.put(name, id);
+                                        info = info + ("            Nom de test : '" + name + "' Id de test : '" + id + "'\n");
+                                    }
+                                    System.out.println(info);
+                                    return Tests;
+                                }
+                            }
+                            System.out.println("Erreur : l'iteration avec comme nom " + IterationName + " n'existe pas");
+                            return null;
+                        }
+                    }
+                    System.out.println("Erreur : la campagne avec comme nom " + CampaignName + " n'existe pas");
+                    return null;
                 }
             }
+            System.out.println("Erreur : le projet avec comme nom " + ProjectName + " n'existe pas");
+            return null;
+        } catch (Exception exception) {
+            switch (exception.getClass().getSimpleName()) {
+                case "UnknownHostException" -> {
+                    System.out.println("Erreur : l'hôte renseigné ne réponds pas");
+                }
+                case "ConnectException" -> {
+                    System.out.println("Erreur : le port renseigné est invalide");
+                }
+                case "JsonPathException" -> {
+                    System.out.println("Erreur : le/les identifiant(s) renseigné(s) est/sont invalide(s)");
+                }
 
-            return "";
+            }
         }
-
-        return "";
+        return null;
     }
 
-    public List<String> getTestCasesByProjectId()
-    {
-        return null;
+    public void modifyTestStatus(@NotNull String TestId, @NotNull String Status) {
+        List<String> list = Arrays.asList("NB OF TC", "READY", "RUNNING", "SUCCESS", "FAILURE", "BLOCKED", "UNTESTABLE");
+        if (list.contains(Status)) {
+            try {
+                auth();
+                String jsonString = "{\r\n" +
+                        "    \"status\" : \"" + Status + "\"}";
+
+                RequestSpecification request = RestAssured.given();
+
+                request.contentType(ContentType.JSON);
+                request.baseUri("http://localhost:8080/squash/api/rest/latest/test-suites/" + TestId);
+                request.body(jsonString);
+            } catch (Exception exception) {
+                switch (exception.getClass().getSimpleName()) {
+                    case "UnknownHostException" -> {
+                        System.out.println("Erreur : l'hôte renseigné ne réponds pas");
+                    }
+                    case "ConnectException" -> {
+                        System.out.println("Erreur : le port renseigné est invalide");
+                    }
+                    case "JsonPathException" -> {
+                        System.out.println("Erreur : le/les identifiant(s) renseigné(s) est/sont invalide(s)");
+                    }
+
+                }
+            }
+        } else {
+            System.out.println("Erreur : le status donner en paramêtre n'est pas valide");
+        }
     }
 }
